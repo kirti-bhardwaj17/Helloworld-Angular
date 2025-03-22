5fe9f6f738026241dd1b311890c51fb8c986d8a6
kirti-bhardwaj17 <bhardwajkirti17@gmail.com>
2025-03-22 16:18:20 +0530

    To display Hello from BridgeLabz with a user inputted name

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Helloworld';
-  imgUrl="../assets/BL_logo_square_jpg.jpg"
+  imgUrl = "../assets/BL_logo_square_jpg.jpg";
+  url = "https://www.bridgelabz.com";
+  userName: string = "";
   ngOnInit(): void{
     this.title = "Hello from BridgeLabz.";
   }
